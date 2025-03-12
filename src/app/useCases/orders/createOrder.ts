393268ac5f9c04114bc2ae876c939cf570826e00
Gerson77 <gerson.snts55@gmail.com>
2025-03-11 21:07:05 -0300

    Adicionando o EmplooyId do garçom

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from "express";
 import { Order } from "../../models/Order";
-// import { io } from "../../..";
+import { io } from "../../..";
 
 export async function createOrder(req: Request, res: Response) {
   try {
-    const { table, products } = req.body;
+    const { table, products, employeeId } = req.body;
 
     const order = await Order.create({
       table,
-      products
+      products,
+      employeeId
     });
     const orderDetails = await order.populate('products.product')
 
-    // io.emit('order@new', orderDetails)
+    io.emit('order@new', orderDetails)
     res.status(201).json(order);
   } catch (error) {
     console.log(error)
